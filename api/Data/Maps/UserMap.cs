2432f39f05429a00f2ae2eea81f3b410831ec544
Matheus Christo <matheuschristo.09@gmail.com>
2022-11-29 17:07:17 -0300

    Create UserMap.cs

---
+++ api/Data/Maps/UserMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class UserMap : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(x => x.id);
+        builder.Property(x => x.name).IsRequired().HasMaxLength(50);
+        builder.Property(x => x.username).IsRequired().HasMaxLength(25).IsUnicode();
+        builder.Property(x => x.password).IsRequired();
+    }
+}
+
