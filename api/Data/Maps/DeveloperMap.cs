f5859eb58e64a2e1c35deb16cc8007ea2442e8a3
Matheus Christo <matheuschristo.09@gmail.com>
2022-11-29 17:00:42 -0300

    Create DeveloperMap.cs

---
+++ api/Data/Maps/DeveloperMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class DeveloperMap : IEntityTypeConfiguration<Developer>
+{
+	public void Configure(EntityTypeBuilder<Developer> builder)
+	{
+		builder.HasKey(x => x.id);
+		builder.Property(x => x.name).IsRequired().HasMaxLength(50);	
+		builder.Property(x => x.username).IsRequired().HasMaxLength(25).IsUnicode();
+		builder.Property(x => x.password).IsRequired();
+		builder.Property(x => x.score);
+		builder.HasMany(x => x.courses);
+	}
+}
