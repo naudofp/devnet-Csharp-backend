59c220fe3c505dabdd96654482d3f20957262c9a
Matheus Christo <matheuschristo.09@gmail.com>
2022-11-29 15:58:12 -0300

    Create Course.cs

---
+++ api/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace devnet_Csharp_backend.api.Models
+{
+	public class Course
+	{
+		public long id { get; set; }
+		
+		public string name { get; set; }
+
+		public readonly List<Developer> developers { get; }
+	}
+}
