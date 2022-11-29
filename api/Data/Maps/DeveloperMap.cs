c5aac5e601295a7b6ba1c4eb06513121049dc49f
Matheus Christo <matheuschristo.09@gmail.com>
2022-11-29 17:07:27 -0300

    Update DeveloperMap.cs

--- api/Data/Maps/DeveloperMap.cs
+++ api/Data/Maps/DeveloperMap.cs
@@ -6,10 +6,6 @@ public class DeveloperMap : IEntityTypeConfiguration<Developer>
 {
 	public void Configure(EntityTypeBuilder<Developer> builder)
 	{
-		builder.HasKey(x => x.id);
-		builder.Property(x => x.name).IsRequired().HasMaxLength(50);	
-		builder.Property(x => x.username).IsRequired().HasMaxLength(25).IsUnicode();
-		builder.Property(x => x.password).IsRequired();
 		builder.Property(x => x.score);
 		builder.HasMany(x => x.courses);
 	}
