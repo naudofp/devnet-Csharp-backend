51573a40ab5a1d7f729eb0254ed72df3557c23c0
Matheus Christo <matheuschristo.09@gmail.com>
2022-11-29 16:34:20 -0300

    Update Course.cs

--- api/Models/Course.cs
+++ api/Models/Course.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Threading.Tasks;
 
 public class Course
 {
 	public long id { get; set; }
+
 	public string name { get; set; }
+
 	public int addScore { get; set; }
+
 	public ICollection<Developer> developers { get; }
 }
 
