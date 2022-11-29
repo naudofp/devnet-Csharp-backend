9de011b1ac580aa4e5cfe1f181a5e7cf547275ec
naudofp <felipenaudof@gmail.com>
2022-11-29 15:45:57 -0300

    Create User and Developer

--- api/Models/Developer.cs
+++ api/Models/Developer.cs
@@ -5,10 +5,7 @@ using System.Threading.Tasks;
 
 namespace devnet_Csharp_backend.api.Models
 {
-    abstract class User
+    public class Developer 
     {
-        public long id { get; set; }
-        public string? name { get; set; }
-        public string? username { get; set; }
     }
 }
