52be0291e15b1534a3b4a0ec08a52c210e6ab5ce
naudofp <felipenaudof@gmail.com>
2022-11-29 15:32:22 -0300

    Create Folders

---
+++ api/Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace devnet_Csharp_backend.api.Models
+{
+    public class User
+    {
+        
+    }
+}
