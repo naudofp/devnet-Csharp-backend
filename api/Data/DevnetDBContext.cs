9bd27aed1476565a48a8b31e6635cc31ffd8f62e
naudofp <felipenaudof@gmail.com>
2022-11-29 16:54:07 -0300

    Create DevnetDBContext

--- api/Data/DevnetDBContext.cs
+++ api/Data/DevnetDBContext.cs
@@ -2,8 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-public class DevnetDBContext
+public class DevnetDBContext : DbContext
 {
-            
+    public DevnetDBContext(DbContextOptions<DevnetDBContext> options) : base(options) {}
+
+    public DbSet<Developer> developer { get; set; }
+    public DbSet<User> user { get; set; }
+    public DbSet<Course> course { get; set; }
+
+     protected override void OnModelCreating(ModelBuilder modelBuilder){
+
+            base.OnModelCreating(modelBuilder);   
+        }
 }
